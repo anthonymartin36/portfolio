1a6d0b54332267f060ba547b195f9b633a3ec517
anthonymartin36 <anthony.martin36@gmail.com>
2024-07-09 16:19:06 +1200

    base email sending

--- client/components/Contact.tsx
+++ client/components/Contact.tsx
@@ -1,10 +1,34 @@
 import './Contact.css'
-//import { Link } from 'react-router-dom' 
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+// import * as dotenv from 'dotenv'
+
+// config(dotenv)
 
 import logoGithub from '../images/technology/LinkedIn_icon.svg'
 import logoLinkedIn from '../images/technology/github-icon.svg'
 
 export default function Contact() {
+    
+    const form = useRef()
+    
+    const sendEmail = (e: any) => {
+        e.preventDefault()
+    
+        emailjs
+          .sendForm(import.meta.env.VITE_EMAIL_SERVICE_ID, import.meta.env.VITE_EMAIL_TEMPLATE_ID, form.current, {
+            publicKey: import.meta.env.VITE_PUBLIC_KEY
+        })
+        .then(
+            () => {
+              console.log('SUCCESS!')
+            },
+            (error: any) => {
+              console.log('FAILED...', error.text)
+            },
+        )
+    }
+
     return (
         <>
         <div className='contact' id='contact'>
@@ -15,21 +39,6 @@ export default function Contact() {
                     <div className='row-1'>
                         <p>Reach out to me </p>
                     </div>
-                    <div className='row-3'>
-                        <form action=''>
-                            <div className='form'>
-                                <h2>Contact Me</h2>
-                                <p>Email Address: </p> 
-                                <input type='text' name='email' />
-                                <p>Message Body: </p>
-                                <textarea name='message' /> 
-                                <div className='btn'>
-                                <input type='submit' value='Submit' />
-                                <input type="reset" value="Reset"/>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
                     <div className='row-2'>
                         <div className='logo-image'>
                             <a href='https://www.linkedin.com/in/anthonymartinire/' ><img src={`${logoLinkedIn}`} alt='LinkedIn' /></a>
@@ -37,6 +46,26 @@ export default function Contact() {
                          <div className='logo-image'>
                          <a href='https://github.com/anthonymartin36/' ><img src={`${logoGithub}`}  alt='Github' /></a>
                          </div>
+                    </div>
+                    <div className='row-3'>
+                        <form ref={form} onSubmit={sendEmail}>
+                            <div className='form'>
+                                <h2>Contact Me</h2>
+                                <p>Email Address: </p> 
+                                <input type='text' name='user_email' />
+                                <p>Your Name: </p> 
+                                <input type='text' name='user_name' />
+                                <p>Message Body: </p>
+                                <textarea name='message' /> 
+                                <div className='reCAPTCHA'>
+                                reCAPTCHA    
+                                </div>
+                                <div className='btn'>
+                                <input type='submit' value='Send' />
+                                <input type="reset" value="Reset"/>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>  
             </div>
